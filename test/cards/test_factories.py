5be0e670fbf330f764bc89912187ace9e4889577
Fabien Roy <fabienroy28@gmail.com>
2020-04-11 14:44:20 -0400

    Fix lint

--- test/cards/test_factories.py
+++ test/cards/test_factories.py
@@ -1,7 +1,6 @@
 import unittest
 from unittest import mock
 
-from app.cards.enums import HouseValue, HouseSuit
 from app.cards.factories import NORMAL_DECK_LENGTH, DeckFactory
 from app.cards.models import Card
 
